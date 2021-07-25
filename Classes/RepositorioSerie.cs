1ed488c9c8049c67d4c3671fa0ba16758aef6dbc
acpfm <ana.pasti.ferreira@gmail.com>
2021-07-25 12:05:31 -0300

    Projeto Cadastro de Series, produzido durante curso de c# e .net (console).

---
+++ Classes/RepositorioSerie.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using CadastroSeries.Interfaces;
+
+namespace CadastroSeries
+{
+    public class RepositorioSerie : IRepositorio<Serie>
+    {
+        
+    }
+}
